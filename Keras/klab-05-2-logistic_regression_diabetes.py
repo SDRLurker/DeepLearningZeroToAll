9989760f4a837053abfeaa4726c8e2bbcfd4c016
Ryan Shin <1982sdr@hanmail.net>
2017-05-09 11:04:47 +0900

    modify klab-05-2-logistic_regression_diabetes.py (#159)

--- Keras/klab-05-2-logistic_regression_diabetes.py
+++ Keras/klab-05-2-logistic_regression_diabetes.py
@@ -3,27 +3,15 @@ from keras.layers import Dense
 from keras.optimizers import SGD
 import numpy as np
 
-x_data = [[1, 2],
-          [2, 3],
-          [3, 1],
-          [4, 3],
-          [5, 3],
-          [6, 2]]
-y_data = [[0],
-          [0],
-          [0],
-          [1],
-          [1],
-          [1]]
+xy = np.loadtxt('data-03-diabetes.csv', delimiter=',')
+x_data = xy[:, 0:-1]
+y_data = xy[:, [-1]]
 
 model = Sequential()
-model.add(Dense(1, input_dim=2, activation='sigmoid'))
+model.add(Dense(1, input_dim=8, activation='sigmoid'))
 
 sgd = SGD(lr=0.1)
-model.compile(loss='binary_crossentropy', optimizer=sgd)
+model.compile(loss='binary_crossentropy', optimizer=sgd, metrics=['accuracy'])
 
 model.summary()
-model.fit(x_data, y_data, epochs=2000)
-
-print("2,1", model.predict_classes(np.array([[2, 1]])))
-print("6,5", model.predict_classes(np.array([[6, 5]])))
+model.fit(x_data, y_data, epochs=1000)
